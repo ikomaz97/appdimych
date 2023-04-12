2e33a75bb71b86d1cc4100580e1323269f498732
ikomaz97 <ikomaz97@gmail.com>
2023-04-12 13:41:04 +0400

    12/04/04

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -3,14 +3,26 @@ type RatingPropsType = {
     value: 0|1|2|3|4|5
 }
 export function Rating(props: RatingPropsType) {
-    if (props.value === 1) {
+    if (props.value === 0) {
         return (
             <div>
                 <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+
+    }
+    if (props.value === 1) {
+        return (
+            <div>
                 <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
             </div>
         );
 
@@ -18,11 +30,11 @@ export function Rating(props: RatingPropsType) {
     if (props.value === 2) {
         return (
             <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
                 <Star selected={false}/>
                 <Star selected={false}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
+                <Star selected={false}/>
             </div>
         );
 
@@ -30,11 +42,11 @@ export function Rating(props: RatingPropsType) {
     if (props.value === 3) {
         return (
             <div>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
                 <Star selected={true}/>
                 <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
             </div>
         );
 
@@ -42,11 +54,11 @@ export function Rating(props: RatingPropsType) {
     if (props.value === 4) {
         return (
             <div>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
                 <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
             </div>
         );
 
@@ -54,11 +66,11 @@ export function Rating(props: RatingPropsType) {
 
     return (
         <div>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
         </div>
     );
 
