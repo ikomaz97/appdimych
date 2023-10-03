8e8fa6bac5a14ad2d0a8116ecc78d7e0fd9e29a9
ikomaz97 <ikomaz97@gmail.com>
2023-10-03 16:44:35 +0400

    03/10/23-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,31 @@
-import React from 'react';
-import './App.css';
-import Accordion from './components/Accordion/Accordion';
-import {Rating} from "./components/Rating/Rating";
+import React, { useState } from "react";
+import "./App.css";
+import Accordion from "./components/Accordion/Accordion";
+import { Rating, RatingValueType } from "./components/Rating/Rating";
 import OnOff from "./components/OnOff/OnOff";
 import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
-import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
-function App(props:any) {
-       return (
-        <div className={'App'}>
-            <OnOff/>
-           {/* <PageTitle title={"This is App component"}/>
-            <PageTitle title={"My frends"}/>*/}
-            {/*<UncontrolledRating value = {0}/>
-            <UncontrolledRating value = {1}/>
-            <UncontrolledRating value = {2}/>
-            <UncontrolledRating value = {3}/>
-            <UncontrolledRating value = {4}/>
-            <UncontrolledRating value = {5}/>
-            <Accordion titleValue = "Menu"/>
-            <Accordion titleValue = "Users"/>*/}
-            <UncontrolledAccordion titleValue = "Menu"/>
-            <Rating value = {3}/>
+import { UncontrolledRating } from "./components/UncontrolledRating/UncontrolledRating";
 
-                   </div>
+function App(props: any) {
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0);
 
+    return (
+        <div className={"App"}>
+            <OnOff />
+            <UncontrolledAccordion titleValue="Menu" />
+            <Rating value={ratingValue} onClick={setRatingValue} />
+            <UncontrolledRating value={ratingValue} />
+            {/*<Accordion titleValue ={'Menu'} />*/}
+        </div>
     );
 }
+
 type PageTitlePropsType = {
-    title: string
-}
+    title: string;
+};
 
 function PageTitle(props: PageTitlePropsType) {
-    return (
-        <h1>{props.title}</h1>
-    );
+    return <h1>{props.title}</h1>;
 }
-
 
 export default App;
