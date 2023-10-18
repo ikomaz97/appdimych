ab12561b612fc3ec57695616ab907e3abe933c4b
ikomaz97 <ikomaz97@gmail.com>
2023-10-18 14:38:02 +0400

    117/10/23-2

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -28,7 +28,7 @@ type AccordionTitleProps = {
 function AccordionTitle(props: AccordionTitleProps) {
     return (
         <div>
-            <h3 onClick={props.onChange}>---{props.title}---</h3>
+            <h3 onClick={(event)=>props.onChange()}>---{props.title}---</h3>
         </div>
     );
 }
