f07e2c8389746a1badce2efaee3f284d664d31a0
ikomaz97 <ikomaz97@gmail.com>
2023-10-18 14:38:37 +0400

    117/10/23-5

--- src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-type PropsType ={
-  /*  on:boolean,*/
+export type PropsType ={
+  onChange:(on:boolean)=>void
 }
 
 
@@ -37,10 +37,21 @@ const UncontrolledOnOff = (props:PropsType) => {
         backgroundColor: on ? 'green' : 'red',
     }
 
+    const onClicked = () => {
+        setOn(true)
+        props.onChange(true)
+    }
+const offClicked = () => {
+  setOn(false)
+    props.onChange(false)
+}
+
+
+
     return (
         <div>
-            <div style={onStyle} onClick={()=>{setOn(true)}}>On</div>
-            <div style={offStyle} onClick={()=>{setOn(false)}}>Off</div>
+            <div style={onStyle} onClick={onClicked}>On</div>
+            <div style={offStyle} onClick={offClicked}>Off</div>
             <div style={indicatorStyle} onClick={()=>{}}></div>
         </div>
     );
