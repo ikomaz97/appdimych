d8607d4e12d09c331421e8b4ee84800d889a2b13
ikomaz97 <ikomaz97@gmail.com>
2023-10-22 11:25:09 +0400

    22/10/23-6

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,31 +1,39 @@
-import React, {useState} from "react";
-import OnOff from "../OnOff/OnOff";
+import React, { useState } from "react";
 
 export type RatingPropsType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
-}
+    defaultValue?: number; // Изменил тип defaultValue на number
+    onChange: (value: number) => void; // Изменил тип параметра onChange
+};
 
 export function UncontrolledRating(props: RatingPropsType) {
-    let [value, setValue] = useState(4);
+    let [value, setValue] = useState<number>(props.defaultValue || 0);
     return (
         <div>
-            <Star selected={value > 0} setValue={()=> {setValue(1)}}/>
-            <Star selected={value > 1} setValue={()=> {setValue(2)}}/>
-            <Star selected={value > 2} setValue={()=> {setValue(3)}}/>
-            <Star selected={value > 3} setValue={()=> {setValue(4)}}/>
-            <Star selected={value > 4} setValue={()=> {setValue(5)}}/>
+            <Star selected={value > 0} setValue={() => handleRatingChange(1)} />
+            <Star selected={value > 1} setValue={() => handleRatingChange(2)} />
+            <Star selected={value > 2} setValue={() => handleRatingChange(3)} />
+            <Star selected={value > 3} setValue={() => handleRatingChange(4)} />
+            <Star selected={value > 4} setValue={() => handleRatingChange(5)} />
         </div>
     );
+
+    function handleRatingChange(newValue: number) {
+        if (newValue !== value) {
+            setValue(newValue);
+            props.onChange(newValue);
+        }
+    }
 }
 
 type StarPropsType = {
-    selected: boolean
-    setValue: () => void
-}
+    selected: boolean;
+    setValue: () => void;
+};
 
 function Star(props: StarPropsType) {
-    return <span onClick={() => {
-        props.setValue()
-    }}> {props.selected ? <b>star- </b> : 'star '}</span>
+    return (
+        <span onClick={() => {
+            props.setValue();
+        }}> {props.selected ? <b>star- </b> : 'star '}</span>
+    );
 }
-
