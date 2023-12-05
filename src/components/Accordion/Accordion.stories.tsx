469f834563e26e27a7a5263330ed0bc803a64060
ikomaz97 <ikomaz97@gmail.com>
2023-12-05 17:46:45 +0400

    05/12/23-1

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,25 +1,45 @@
-import type { Meta } from '@storybook/react';
-import { actions } from '@storybook/addon-actions';
+import type {Meta} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
 import Accordion from './Accordion';
 import React, {useState} from "react";
-export default  {
+
+export default {
     component: Accordion,
 };
+
+const callback = action('accordion mode change event fired')
+const onClickCallback = action('same item was clicked')
 export const CollapsedAccordion = () => {
-    return <Accordion  titleValue ={'Menu'}
-                       collapsed={true}
-                       onChange = {()=>{}} />
+    return <Accordion titleValue={'Menu'}
+                      collapsed={true}
+                      onChange={callback} items={[]}
+                      onClick={onClickCallback}
+    />
 };
 
 export const OpenedAccordion = () => {
-    return <Accordion  titleValue ={'Menu'}
-                       collapsed={false}
-                       onChange = {()=>{}} />
+    return <Accordion titleValue={'Users'}
+                      collapsed={false}
+                      onChange={callback}
+                      items={[{title: 'Dimych', value: 1}, {title: "Valera", value: 2}, {
+                          title: 'Artem',
+                          value: 3
+                      }, {title: 'Victor', value: 4},]}
+                      onClick={onClickCallback}
+    />
 };
 
 export const AccordionDemo = () => {
     const [collapsed, setCollapsed] = useState<boolean>(true)
-    return <Accordion  titleValue ={'Accordion'}
-                       collapsed={collapsed}
-                       onChange = {()=>{setCollapsed(!collapsed)}} />
+    return <Accordion titleValue={'Accordion'}
+                      collapsed={collapsed}
+                      onChange={() => {
+                          setCollapsed(!collapsed)
+                      }}
+                      items={[{title: 'Dimych', value: 1}, {title: "Valera", value: 2}, {
+                          title: 'Artem',
+                          value: 3
+                      }, {title: 'Victor', value: 4},]}
+                      onClick={onClickCallback}
+    />
 };
